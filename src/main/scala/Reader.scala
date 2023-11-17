7b53f0ec9ad56f41c1f5af1a129f4a7ed418e102
vorobevjlt <127599181+vorobevjlt@users.noreply.github.com>
2023-11-17 15:45:45 +0300

    Create Reader.scala

---
+++ src/main/scala/Reader.scala
@@ -0,0 +1,130 @@
+object Reader {
+
+  case class Record(
+    @JsonProperty("commentId") var commentId: String,
+    @JsonProperty("time") var time: Long,
+    @JsonProperty("user") var user: String,
+    @JsonProperty("topic") var topic: String,
+    @JsonProperty("acronym") var acronym: String)
+
+  case class Acronym(var date: String, var value: String, var count: Long) {
+    def this() {
+      this("", "", 0L)
+    }
+  }
+
+
+  object DayPart extends DayPart {
+    implicit def dayPartToString(part: DayPart.Value): String = part.toString
+  }
+
+  trait DayPart extends Enumeration {
+    val day, night,
+    evening, morning = Value
+  }
+
+  case class ProcessedRecord(partPOfDay: String, count: Long)
+
+
+  def readCsvStream(): Unit = {
+    val filePath = new Path("src/main/resources/acronyms.csv")
+    val dateFormat = new SimpleDateFormat("yyyy-MM-dd")
+    val quarterDayFormat = new SimpleDateFormat("hh:mm")
+    val hardwareOutputTag = new OutputTag[Record]("hardware"){}
+
+    val targetTopic = "Hardware"
+    val env = StreamExecutionEnvironment
+      .getExecutionEnvironment
+
+    val csvSchema = CsvSchema
+      .builder()
+      .addNumberColumn("commentId")
+      .addColumn("time")
+      .addColumn("user")
+      .addColumn("topic")
+      .addColumn("acronym")
+      .build()
+
+    val source: FileSource[Record] = FileSource
+      .forRecordStreamFormat(
+        CsvReaderFormat.forSchema(csvSchema, Types.GENERIC(classOf[Record])),
+        filePath)
+      .build()
+
+
+    val separateStreamsFunction =
+      new ProcessFunction[Record, Acronym] {
+        override def processElement(
+          value: Record,
+          ctx: ProcessFunction[Record, Acronym]#Context,
+          out: Collector[Acronym]): Unit = {
+
+            if (value.topic == targetTopic) ctx.output(hardwareOutputTag, value)
+
+            out.collect(new Acronym(
+              dateFormat.format(value.time), value.acronym, 1L))
+        }
+      }
+
+
+    val acronymKey = new KeySelector[Acronym, Tuple2[String, String]] {
+      override def getKey(keyValue: Acronym): Tuple2[String, String] =
+        Tuple2.of(keyValue.date, keyValue.value)
+    }
+
+    def getDayPart(time: LocalTime): String = {
+      if (time.compareTo(LocalTime.parse("04:00")) == 1 && time.compareTo(LocalTime.parse("12:00")) == -1)
+        DayPart.morning
+      else if (time.compareTo(LocalTime.parse("12:00")) == 1 && time.compareTo(LocalTime.parse("17:00")) == -1)
+        DayPart.day
+      else if (time.compareTo(LocalTime.parse("17:00")) == 1 && time.compareTo(LocalTime.parse("00:00")) == -1)
+       DayPart.evening
+      else
+        DayPart.night
+    }
+
+    val processHardwareFunction = new ProcessFunction[Record, ProcessedRecord] {
+      override def processElement(
+          value: Record,
+          ctx: ProcessFunction[Record, ProcessedRecord]#Context,
+          out: Collector[ProcessedRecord]): Unit = {
+        val time = LocalTime.parse(quarterDayFormat.format(value.time))
+        val dayPart = getDayPart(time)
+
+        out.collect(ProcessedRecord(dayPart, 1))
+      }
+    }
+
+
+    val stream: DataStreamSource[Record] = env
+      .fromSource(
+        source,
+        WatermarkStrategy.noWatermarks(),
+        "csv-file"
+      )
+
+
+    val separatedStream = stream
+      .process(separateStreamsFunction)
+
+    val countAcronymStream = separatedStream
+      .keyBy(acronymKey)
+      .sum("count")
+
+    val hardwareStream = separatedStream
+      .getSideOutput(hardwareOutputTag)
+
+    val countTargetTopicStream = hardwareStream
+      .process(processHardwareFunction)
+
+   // countTargetTopicStream.print()
+   // countAcronymStream.print()
+
+    env.execute()
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    readCsvStream()
+  }
+}
